1704e52d5fd60daad66a0c012c6b06b9344c3661
Jacobo Arcila Duque <jacoboarcila@hotmail.com>
2018-02-23 17:11:17 -0500

    Taller 2 - Jacobo Arcila Duque

---
+++ Taller 2/Assets/Scripts/Around.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Around : MonoBehaviour {
+	public float speed;
+	public Transform sunPivot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.RotateAround (sunPivot.position, Vector3.up, speed);
+
+	}
+}
